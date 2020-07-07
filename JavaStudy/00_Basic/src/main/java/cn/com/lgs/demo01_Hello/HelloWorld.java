b1a4d14808899d08393f6b7eaf973e3021f7a245
luguosong <1054595718@qq.com>
2020-07-07 18:58:13 +0800

    2020 7.7 18.58

---
+++ JavaStudy/00_Basic/src/main/java/cn/com/lgs/demo01_Hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.com.lgs.demo01_Hello;
+
+/**
+ * @Author luGuoSong
+ * @Date 2020/7/7 15:37
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        int num = 2147483647 ;
+        num += 2L ;
+        System.out.println(num) ;
+    }
+}
