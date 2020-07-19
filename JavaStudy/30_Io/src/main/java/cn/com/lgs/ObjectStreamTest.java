e91250ec3fe9bec95eaa9ff2c3f3c3f87134b0ee
guosong lu <1054595718@qq.com>
2020-07-20 00:01:55 +0800

    2002 7.19 23:52

---
+++ JavaStudy/30_Io/src/main/java/cn/com/lgs/ObjectStreamTest.java
@@ -0,0 +1,10 @@
+package cn.com.lgs;
+
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+public class ObjectStreamTest {
+    public static void main(String[] args) {
+        //ObjectOutputStream objectOutputStream=new ObjectOutputStream("");
+    }
+}
--- JavaStudy/30_Io/src/main/java/cn/com/lgs/SystemStream.java
+++ JavaStudy/30_Io/src/main/java/cn/com/lgs/SystemStream.java
@@ -1,9 +1,8 @@
 package cn.com.lgs;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * @Author luGuoSong
@@ -11,11 +10,21 @@ import java.io.InputStreamReader;
  * @Version 1.0
  */
 public class SystemStream {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //自己实现标准键盘输入
         System.out.println("请输入");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("输入的字符为："+bufferedReader.readLine());
 
+        //Scanner类实现了上面相同的功能
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入：");
+        System.out.println("输入的内容为："+scanner.next());
+
+        //标准输出流
+        //本质上是字节打印输出流
+        PrintStream out = System.out;
+        out.println("aaa");
 
     }
 }
