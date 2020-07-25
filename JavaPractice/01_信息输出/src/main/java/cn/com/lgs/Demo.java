36b8cf788744e26a107505f19ef30758b7df6aa3
luguosong <1054595718@qq.com>
2020-07-25 17:51:17 +0800

    2020 7.25 17：50

--- JavaPractice/01_信息输出/src/main/java/cn/com/lgs/Demo.java
+++
@@ -1,7 +0,0 @@
-package cn.com.lgs;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.err.println("111");
-    }
-}
---
+++ JavaPractice/Java1200/src/main/java/cn/com/lgs/chapter2_Java基础应用/demo22_自动类型转换与强制类型转换/Demo.java
@@ -0,0 +1,16 @@
+package cn.com.lgs.chapter2_Java基础应用.demo22_自动类型转换与强制类型转换;
+
+/**
+ * @Author luGuoSong
+ * @Date 2020/7/25 15:31
+ * @Version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        byte b=127;
+        char c='W';
+
+        //低类型向高类型自动转换
+
+    }
+}
