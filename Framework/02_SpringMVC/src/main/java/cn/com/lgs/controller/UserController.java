319c7d0837efee573479fe7d54677d238a3beb98
luguosong <1054595718@qq.com>
2020-09-06 18:15:54 +0800

    2020 9.6 18:15

--- Framework/02_SpringMVC/src/main/java/cn/com/lgs/controller/UserController.java
+++ Framework/02_SpringMVC/src/main/java/cn/com/lgs/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.com.lgs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @Author luGuoSong
@@ -11,9 +12,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
+
+    /**
+     * 直接返回字符串进行页面跳转
+     * @return 跳转地址
+     */
     @RequestMapping("/user")
     public String save(){
         System.out.println("Controller执行");
-        return "success.jsp";
+        return "success";
     }
+
+    /**
+     * 返回ModelAndView进行页面跳转
+     * @param model
+     * @return
+     */
+    @RequestMapping("/user2")
+    public ModelAndView save2(ModelAndView model){
+        model.setViewName("success");
+        return model;
+    }
+
+
 }
--- JavaPractice/Java1200/src/main/java/cn/com/lgs/chapter2_Java基础应用/demo28_判断今年是否为闰年/Demo.java
+++ JavaPractice/Java1200/src/main/java/cn/com/lgs/chapter2_Java基础应用/demo28_判断今年是否为闰年/Demo.java
@@ -1,5 +1,7 @@
 package cn.com.lgs.chapter2_Java基础应用.demo28_判断今年是否为闰年;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.util.Scanner;
 
 /**
@@ -10,12 +12,24 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入一个年份：");
-        long year = scanner.nextLong();
-        if (year%4==0&&year%100!=0||year%400==0){
-            System.out.println("是闰年");
-        }else{
-            System.out.println("不是闰年");
+        while (true) {
+            System.out.println("请输入一个年份：");
+            String line = scanner.nextLine();
+            if ("exit".equals(line)) {
+                break;
+            }
+            int year = 0;
+            try {
+                year=Integer.parseInt(line);
+            }catch (NumberFormatException e){
+                System.out.println("你输入的不是一个数字");
+                continue;
+            }
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println("是闰年");
+            } else {
+                System.out.println("不是闰年");
+            }
         }
     }
 }
